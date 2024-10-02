faa18757f519011a747804586ba392c1ab153300
Gabriel Costa <gahluc542@gmail.com>
2024-10-02 16:38:11 -0300

    Stating to implement Board and Piece

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import boardgame.Board;
 import boardgame.Position;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -7,8 +8,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8,8);
+
 
 
     }
