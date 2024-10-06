cb6ac8d7dd0b246ee660eb22b75ab78db5fd620a
Gabriel Costa <gahluc542@gmail.com>
2024-10-05 21:51:54 -0300

    Progra. Defensive

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import static java.lang.constant.ConstantDescs.NULL;
+
 public class Board {
     private  int rows;
     private  int columns;
@@ -7,8 +9,7 @@ public class Board {
 
     public Board(int rows, int columns){
 
-        this.setRows(rows);
-        this.setColumns(columns);
+
         pieces = new Piece[rows][columns];
     }
 
@@ -16,19 +17,18 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
+
 
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
+
 
     public Piece piece (int row, int column){
+        if(!positionExists(row,column)){
+            throw new IllegalArgumentException("Position not on the board ");
+        }
         return pieces[row][column];
     }
     public Piece piece(Position position){
@@ -38,5 +38,15 @@ public class Board {
     public void placePiece(Piece piece, Position position){
         pieces[position.getRow()][position.getColumn()] =piece;
         piece.position =position;
+    }
+
+    private boolean positionExists(int row, int column){
+      return row >=0 && row < row && column >=0 && column <column;
+    }
+    public boolean positionExists(Position position){
+    return positionExists(position.getRow(),position.getColumn());
+    }
+    public boolean thereISAPiece(Position position){
+      return piece(position)!= NULL;
     }
 }
